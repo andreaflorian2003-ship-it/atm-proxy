1ac0892f8ce31b17b45f2b23ba6b91df63eee198
andreaflorian2003-ship-it <andrea.florian2003@gmail.com>
2026-06-14 23:51:56 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+{
+  "name": "atm-proxy",
+  "version": "1.0.0",
+  "description": "Proxy ATM Milano per ESP32",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "engines": {
+    "node": ">=18"
+  }
+}
